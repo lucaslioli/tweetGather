fb4b01dc422df83b12a8f4535029e7719dd535c8
lucaslioli <loliveira@inf.ufsm.br>
2017-12-18 16:50:33 -0200

    Streaming algorithm updated

--- extract/streamListener.py
+++ extract/streamListener.py
@@ -5,6 +5,57 @@ import datetime
 from authenticate import api_tokens
 from db_connection import DbConnecion
 
+#override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+	
+	def on_status(self, status):
+		print("-----------------------------------------")
+		print("Date:", status.created_at)
+		print("User:", status.author.screen_name)
+		print("Text:", status.text, "\n")
+
+		user_insert = {}
+		user_insert['id']            = status.author.id
+		user_insert["name"]          = status.author.name
+		user_insert["screen_name"]   = status.author.screen_name
+		user_insert['friends_count'] = status.author.friends_count
+		user_insert["lang"]          = status.author.lang
+
+		conn.insert_user(user_insert)
+
+		tweet_insert = {}
+		tweet_insert["id"]                    = status.id
+		tweet_insert["text"]                  = status.text
+		tweet_insert["timestamp_ms"]          = status.created_at
+		tweet_insert["lang"]                  = status.lang
+		tweet_insert["retweet_count"]         = status.retweet_count
+		tweet_insert["favorite_count"]        = status.favorite_count
+		tweet_insert["reply_count"]           = status.reply_count
+		tweet_insert["in_reply_to_status_id"] = status.in_reply_to_status_id
+		tweet_insert["user_id"]               = status.author.id
+		tweet_insert["followers_count"]       = status.author.followers_count
+
+		conn.insert_tweet(tweet_insert)
+
+		return True; # Don't kill the stream
+
+	def on_error(self, status_code):
+		print >> sys.stderr, 'Encountered error with status code:', status_code
+		return True # Don't kill the stream
+
+	def on_timeout(self):
+		print >> sys.stderr, 'Timeout...'
+		return True # Don't kill the stream
+
+# Start the Stream Listener
+def start_stream():
+	while True:
+		try:
+			myStream = tweepy.streaming.Stream(auth, MyStreamListener())
+			myStream.userstream(_with='followings')
+		except:
+			continue
+
 # Variables that contains the user credentials to access Twitter API
 if(len(sys.argv) < 2):
 	print ("You need to inform the user credentials to access.")
@@ -27,50 +78,10 @@ if __name__ == '__main__':
 
 	conn = DbConnecion()
 
-	#override tweepy.StreamListener to add logic to on_status
-	class MyStreamListener(tweepy.StreamListener):
-		
-		def on_status(self, status):
-			print(status.author.screen_name, status.created_at, status.text, "\n\n")
+	start_stream()
 
-			user_insert = {}
-			user_insert['id']            = status.author.id
-			user_insert["name"]          = status.author.name
-			user_insert["screen_name"]   = status.author.screen_name
-			user_insert['friends_count'] = status.author.friends_count
-			user_insert["lang"]          = status.author.lang
+	# user  = api.me()
 
-			conn.insert_user(user_insert)
-
-			tweet_insert = {}
-			tweet_insert["id"]                    = status.id
-			tweet_insert["text"]                  = status.text
-			tweet_insert["timestamp_ms"]          = status.created_at
-			tweet_insert["lang"]                  = status.lang
-			tweet_insert["retweet_count"]         = status.retweet_count
-			tweet_insert["favorite_count"]        = status.favorite_count
-			tweet_insert["reply_count"]           = status.reply_count
-			tweet_insert["in_reply_to_status_id"] = status.in_reply_to_status_id
-			tweet_insert["user_id"]               = status.author.id
-			tweet_insert["followers_count"]       = status.author.followers_count
-
-			conn.insert_tweet(tweet_insert)
-
-			return True; # Don't kill the stream
-
-		def on_error(self, status_code):
-			print >> sys.stderr, 'Encountered error with status code:', status_code
-			return True # Don't kill the stream
-
-		def on_timeout(self):
-			print >> sys.stderr, 'Timeout...'
-			return True # Don't kill the stream
-
-	user  = api.me()
-	
-	follow_list = []
-	for friend in user.friends():
-		follow_list.append(str(friend.id))
-
-	myStream = tweepy.streaming.Stream(auth, MyStreamListener())
-	myStream.userstream(_with='followings')
+	# follow_list = []
+	# for friend in user.friends():
+	# 	follow_list.append(str(friend.id))
